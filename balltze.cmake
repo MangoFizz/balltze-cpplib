ac958710c69ccc3a563db125ce71eab286d312d6
MangoFizz <39842292+MangoFizz@users.noreply.github.com>
2024-07-30 03:20:27 +0000

    Merge Lua and FMT libraries

--- balltze.cmake
+++ balltze.cmake
@@ -7,6 +7,4 @@ include_directories(BEFORE ${CMAKE_CURRENT_LIST_DIR}/include)
 add_library(balltze INTERFACE)
 target_link_libraries(balltze INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/lib/libballtze.dll.a 
-    ${CMAKE_CURRENT_LIST_DIR}/lib/libfmt.a
-    ${CMAKE_CURRENT_LIST_DIR}/lib/liblua53.a
 )
