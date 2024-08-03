9eb3b65aab3938e7c81dfe1ae083fc3ef893fb78
MangoFizz <39842292+MangoFizz@users.noreply.github.com>
2024-08-03 13:28:57 +0000

    Fmt library does not export symbols so static library should be included

--- balltze.cmake
+++ balltze.cmake
@@ -7,4 +7,5 @@ include_directories(BEFORE ${CMAKE_CURRENT_LIST_DIR}/include)
 add_library(balltze INTERFACE)
 target_link_libraries(balltze INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/lib/libballtze.dll.a 
+    ${CMAKE_CURRENT_LIST_DIR}/lib/libfmt.a 
 )
